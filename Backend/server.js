1713834bba5da138f2a56bd56b5b5b192195d0a1
Sanjana-146 <Sanjanaap146@gmail.com>
2025-11-05 18:58:43 +0530

    change cors origin

--- Backend/server.js
+++ Backend/server.js
@@ -13,12 +13,12 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://ai-interview-platform-frontend.onrender.com']
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: allowedOrigins,
     credentials: true,
 }));
 
